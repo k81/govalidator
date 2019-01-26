67cf5c6eb3571ebca86de3a0c8694a302ccdb30e
kaizhangkai <kaizhangkai@didichuxing.com>
2019-01-26 14:12:12 +0800

    initial upload

---
+++ types.go
@@ -0,0 +1,11 @@
+package govalidator
+
+// ParamTagMap is a map of functions accept variants parameters
+//var ParamTagMap = map[string]ParamValidator{
+//"length":       ByteLength,
+//"range":        Range,
+//"stringlength": StringLength,
+//"matches":      StringMatches,
+//"in":           isInRaw,
+//"rsapub":       IsRsaPub,
+//}
